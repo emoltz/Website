d3ce20adbe238cd1bd4b550f066bc9a4d56e2544
emoltz <es5541@nyu.edu>
2022-07-21 12:48:27 -0400

    image

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,11 @@
+from datetime import datetime, date
+import calendar
+
 from django.shortcuts import render
 from django.views.generic import *
 
 
 class Home(TemplateView):
     template_name = 'frontend/welcome.html'
-    extra_context = {}
+    extra_context = {'weekday': calendar.day_name[date.today().weekday()]}
 
