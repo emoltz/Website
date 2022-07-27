5eb0204e610430a14616464fbeb0b0d3666d803b
emoltz <es5541@nyu.edu>
2022-07-27 14:38:08 -0400

    str function

--- blogposts/models.py
+++ blogposts/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.name
+
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
