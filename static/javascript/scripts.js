d708573e8f8b854796526e12990733f5bf54bb9a
emoltz <es5541@nyu.edu>
2022-07-29 11:29:01 -0400

    switcher working

--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -1,1 +1,30 @@
 console.log("https://github.com/emoltz/Website")
+
+const switcher = document.getElementById('theme-switcher')
+const allContent = document.getElementById('theme')
+
+
+function darkMode(){
+     allContent.classList.remove('bg-light');
+        allContent.classList.add('bg-dark');
+        allContent.classList.add('text-white');
+}
+
+function lightMode(){
+    allContent.classList.remove('bg-dark');
+        allContent.classList.add('bg-light');
+        allContent.classList.remove('text-white');
+}
+
+// TODO how to make this persist between pages?
+
+switcher.addEventListener('change', function (event) {
+    if (event.target.checked) {
+        // dark mode activate
+       darkMode();
+
+    } else {
+        // light mode activate
+        lightMode();
+    }
+})
