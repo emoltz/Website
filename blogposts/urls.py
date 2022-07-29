746c09aaa8e28fc962691d7d377d1fbec4066819
emoltz <es5541@nyu.edu>
2022-07-29 16:35:35 -0400

    detail view fixes

--- blogposts/urls.py
+++ blogposts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.BlogHomepage.as_view()),
+    path('', views.BlogHomepage.as_view(), name="blog_home"),
     path('<int:pk>', views.BlogDetail.as_view(), name="blog_detail")
 ]
--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -3,7 +3,7 @@ console.log("https://github.com/emoltz/Website")
 const switcher = document.getElementById('theme-switcher')
 const allContent = document.getElementById('theme')
 const rootSCSS = document.querySelector(':root');
-
+const backButton = document.getElementById('back');
 
 
 function darkMode() {
@@ -11,16 +11,21 @@ function darkMode() {
     allContent.classList.add('bg-dark');
     allContent.classList.add('text-white');
     rootSCSS.style.setProperty('--color-primary', '#81b29a')
+    backButton.classList.toggle('btn-outline-secondary');
+    backButton.classList.toggle('btn-outline-success');
 }
 
 function lightMode() {
     allContent.classList.remove('bg-dark');
     allContent.classList.add('bg-light');
     allContent.classList.remove('text-white');
-    rootSCSS.style.setProperty('--color-primary', '#656d4a')
+    rootSCSS.style.setProperty('--color-primary', '#656d4a');
+    backButton.classList.toggle('btn-outline-secondary');
+    backButton.classList.toggle('btn-outline-success');
 }
 
 // TODO how to make this persist between pages?
+
 
 switcher.addEventListener('change', function (event) {
     if (event.target.checked) {
