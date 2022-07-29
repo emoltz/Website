405cc649259c536bdbb4e6637231a97ff77a319e
emoltz <es5541@nyu.edu>
2022-07-29 12:26:06 -0400

    bugs

--- static/javascript/scripts.js
+++ static/javascript/scripts.js
@@ -2,18 +2,22 @@ console.log("https://github.com/emoltz/Website")
 
 const switcher = document.getElementById('theme-switcher')
 const allContent = document.getElementById('theme')
+const rootSCSS = document.querySelector(':root');
 
 
-function darkMode(){
-     allContent.classList.remove('bg-light');
-        allContent.classList.add('bg-dark');
-        allContent.classList.add('text-white');
+
+function darkMode() {
+    allContent.classList.remove('bg-light');
+    allContent.classList.add('bg-dark');
+    allContent.classList.add('text-white');
+    rootSCSS.style.setProperty('--color-primary', '#81b29a')
 }
 
-function lightMode(){
+function lightMode() {
     allContent.classList.remove('bg-dark');
-        allContent.classList.add('bg-light');
-        allContent.classList.remove('text-white');
+    allContent.classList.add('bg-light');
+    allContent.classList.remove('text-white');
+    rootSCSS.style.setProperty('--color-primary', '#656d4a')
 }
 
 // TODO how to make this persist between pages?
@@ -21,7 +25,7 @@ function lightMode(){
 switcher.addEventListener('change', function (event) {
     if (event.target.checked) {
         // dark mode activate
-       darkMode();
+        darkMode();
 
     } else {
         // light mode activate
