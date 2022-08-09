0104e0ba122554376824883cacfa3b57bd47ce39
emoltz <es5541@nyu.edu>
2022-08-09 19:26:00 -0400

    duplicate files fixed

--- config 2.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    entry: './src/3d.js',
-    mode: 'development',
-    output: {
-        filename: 'index.js',
-        path: '/Users/ethanshafranmoltz/Documents/03_CODING/03_PROJECTS/blog_django/static/javascript'
-    }
-}
