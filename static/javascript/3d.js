ecbcc3b15c91a35708ead68618a06b0a3ea54b44
emoltz <es5541@nyu.edu>
2022-08-10 10:40:33 -0400

    things are working but the textures are having trouble being linked

--- static/javascript/3d.js
+++ static/javascript/3d.js
@@ -1,5 +1,24 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+// import * as dat from 'lil-gui'
+import gsap from 'gsap'
+
+/**
+ * Debug
+ */
+// const gui = new dat.GUI()
+
+const parameters = {
+    materialColor: '#a1a29a'
+}
+
+// gui
+//     .addColor(parameters, 'materialColor')
+//     .onChange(() => {
+//             material.color.set(parameters.materialColor);
+//             particlesMaterial.color.set(parameters.materialColor)
+//         }
+//     )
+
 
 /**
  * Base
@@ -9,15 +28,83 @@ const canvas = document.querySelector('canvas.webgl')
 
 // Scene
 const scene = new THREE.Scene()
-scene.background = true;
 
 /**
- * Object
+ * Test cube
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshMatcapMaterial({ color: 0xffffff })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+
+//materials/texture
+
+const textureLoader = new THREE.TextureLoader();
+const gradientTexture = textureLoader.load('static/images/3d_assets/textures/gradients/3.jpg');
+gradientTexture.magFilter = THREE.NearestFilter;
+
+
+const material = new THREE.MeshToonMaterial(
+    {
+        color: parameters.materialColor,
+        gradientMap: gradientTexture
+    }
+)
+
+const objectsDistance = 4;
+
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, .4, 16, 60),
+    material
+)
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(.8, .35, 100, 16),
+    material
+)
+
+mesh1.position.y = -0;
+mesh2.position.y = -objectsDistance;
+mesh3.position.y = -objectsDistance * 2;
+
+mesh1.position.x = 1;
+mesh2.position.x = -1;
+mesh3.position.x = 1;
+
+
+//LIGHTS
+
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0);
+
+scene.add(mesh1, mesh2, mesh3, directionalLight);
+
+const sectionMeshes = [mesh1, mesh2, mesh3];
+
+//PARTICLES!
+
+const particlesCount = 200;
+const positions = new Float32Array(particlesCount * 3); // 3 because of x, y, z axis
+
+
+for (let i = 0; i < particlesCount; i++) {
+    positions[i * 3] = (Math.random() - .5) * 10; //x
+    positions[i * 3 + 1] = objectsDistance * .5 - Math.random() * objectsDistance * sectionMeshes.length; //y
+    positions[i * 3 + 2] = (Math.random() - .5) * 10; //z
+}
+const particlesGeometry = new THREE.BufferGeometry();
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+//martial
+const particlesMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+});
+
+//points
+const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particles)
+
 
 /**
  * Sizes
@@ -28,50 +115,112 @@ const sizes = {
 }
 
 window.addEventListener('resize', () => {
-    sizes.width = window.innerWidth;
-    sizes.height = window.innerHeight;
-    //update camera aspect ratio
-    camera.aspect = sizes.width / sizes.height;
-    camera.updateProjectionMatrix();
-    //update renderer
-    renderer.setSize(sizes.width, sizes.height)
-});
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
 
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
 
 /**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 3
-scene.add(camera)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+const cameraGroup = new THREE.Group();
+scene.add(cameraGroup);
+
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(
-    Math.min(window.devicePixelRatio, 2)
-)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+//scroll
+
+let scrollY = window.scrollY;
+let currentSection = 0;
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY;
+    const newSection = Math.round(scrollY / sizes.height);
+
+    if (newSection !== currentSection) {
+        currentSection = newSection;
+
+        gsap.to(
+            sectionMeshes[currentSection].rotation,
+            {
+                duration: 1.5,
+                ease: 'power2.inOut',
+                x: '+=6',
+                // y: '+=6',
+            }
+        )
+    }
+
+
+})
+
+//cursor
+
+const cursor = {};
+cursor.x = 0;
+cursor.y = 0;
+
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - .5; //makes it from zero to 1, instead of pixel values. The -.5 makes the center 0
+    cursor.y = event.clientY / sizes.height - .5;
+
+})
 
 /**
  * Animate
  */
 const clock = new THREE.Clock()
+let previousTime = 0;
 
-const tick = () =>
-{
+const tick = () => {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime;
+    previousTime = elapsedTime;
 
-    // Update controls
-    controls.update()
+
+    //animate camera
+    camera.position.y = -scrollY / sizes.height * objectsDistance;
+
+    const parallaxX = cursor.x * .5;
+    const parallaxY = -cursor.y * .5;
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * deltaTime; //easing!
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * deltaTime;
+
+    //animate meshes
+
+
+    // for (const mesh of sectionMeshes) {
+    //     mesh.rotation.x += deltaTime * .1;
+    //     mesh.rotation.y += deltaTime * .12;
+    // }
+
+
+    for (const mesh of sectionMeshes) {
+        mesh.rotation.x = scrollY / sizes.height * objectsDistance *.5;
+        mesh.rotation.y = scrollY / sizes.height * objectsDistance *.5;
+    }
 
     // Render
     renderer.render(scene, camera)
