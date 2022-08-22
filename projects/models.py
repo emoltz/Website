c9b4906dba4de86e44265470e30089037e8c8115
emoltz <es5541@nyu.edu>
2022-08-22 14:59:30 -0400

    unique fields

--- projects/models.py
+++ projects/models.py
@@ -2,16 +2,15 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=255)
-    body = models.TextField()
+    title = models.CharField("Title of Project", max_length=255)
+    body = models.TextField("Description of Project")
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     tags = models.ManyToManyField('Tags', related_name='projects', default='')
     image = models.ImageField(upload_to='uploaded_images/', default=None, null=True, blank=True)
 
     # unique fields:
-    # progress bar
-    # open issues from github
+    github_link = models.URLField("Github Link", default=None)
 
     class Meta:
         verbose_name = "Project"
