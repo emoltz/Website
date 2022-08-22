5403136ac2f7dc156b8929ceac32e53a6dc73821
emoltz <es5541@nyu.edu>
2022-08-22 14:51:32 -0400

    basic models

--- blogposts/models.py
+++ blogposts/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django import template
 
 
 class Category(models.Model):
---
+++ projects/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tags', related_name='projects', default='')
+    image = models.ImageField(upload_to='uploaded_images/', default=None, null=True, blank=True)
+
+    # unique fields:
+    # progress bar
+    # open issues from github
+
+    class Meta:
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
+
+
+class Tags(models.Model):
+    # tags can be the tech stacks I am using for the project, e.g. React, Django, etc.
+    tag_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.tag_name
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
